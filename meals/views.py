7d8c82678a1fb4c77e093ed168a585b3fd76e3f4
C1028030 <c1028030@hallam.shu.ac.uk>
2026-08-19 21:36:14 +0100

    Created initial homepage template

--- meals/views.py
+++ meals/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Homepage
 def home(request):
-    return HttpResponse("<h1>StudentPlate prototype<h1>")
+    return render(request, "meals/home.html")
