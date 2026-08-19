fcf733f35502f7769f95c6284821d9015028de79
C1028030 <c1028030@hallam.shu.ac.uk>
2026-08-19 22:48:58 +0100

    Added initial meal catalogue

--- meals/urls.py
+++ meals/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     # Empty path is homepage
     path("", views.home, name="home"),
+
+    # Displays the complete meal catalogue
+    path("meals/", views.meal_list, name="meal_list"),
 ]
--- meals/views.py
+++ meals/views.py
@@ -3,3 +3,44 @@ from django.shortcuts import render
 # Homepage
 def home(request):
     return render(request, "meals/home.html")
+
+# Displays available meals
+def meal_list(request):
+    # Temporary sample data for first prototype
+    meals = [
+        {
+            "name": "Chicken and vegetable rice bowl",
+            "category": "High protein",
+            "description": "A filling meal made with chicken, rice and vegetables.",
+            "price": 2.35,
+            "prep_time": 25,
+        },
+        {
+            "name": "Spiced chickpea wraps",
+            "category": "Vegetarian",
+            "description": "Quick wraps containing chickpeas, salad and yoghurt.",
+            "price": 1.65,
+            "prep_time": 15,
+        },
+        {
+            "name": "Tuna tomato pasta",
+            "category": "Under £2",
+            "description": "An affordable pasta meal containing tuna and tomatoes",
+            "price": 1.90,
+            "prep_time": 20,
+        },
+        {
+            "name": "Banana overnight oats",
+            "category": "Breakfast",
+            "description": "A simple breakfast that can be prepared the night before",
+            "price": 0.95,
+            "prep_time": 5,
+        },
+    ]
+
+    # Passes the list to the HTML template
+    context = {
+        "meals": meals,
+    }
+
+    return render(request, "meals/meal_list.html", context)
